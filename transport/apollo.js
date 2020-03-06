dc6cb601be02a207a1deefd6086eaefff4eb0d97
Jepser Bernardino <jepsersk8@gmail.com>
2020-03-06 01:02:12 +0100

    feat: add graphql support

---
+++ transport/apollo.js
@@ -0,0 +1,17 @@
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import withApollo from 'next-with-apollo';
+import { createHttpLink } from 'apollo-link-http';
+import fetch from 'isomorphic-unfetch';
+
+export default url =>
+  withApollo(
+    ({ initialState }) =>
+      new ApolloClient({
+        link: createHttpLink({
+          fetch,
+          uri: url
+        }),
+        cache: new InMemoryCache().restore(initialState || {})
+      })
+  );
