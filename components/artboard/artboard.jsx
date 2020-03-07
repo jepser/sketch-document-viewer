2a2e4333fa9dfb497a87cc2bfe89814e601515d3
Jepser Bernardino <jepsersk8@gmail.com>
2020-03-07 15:07:19 +0100

    chore: rename artboard

--- components/artboard/artboard.jsx
+++ components/artboard/artboard.jsx
@@ -12,10 +12,10 @@ import {
 import Spacer from '@components/spacer';
 import Link from 'next/link';
 
-const Artboard = ({ id, name, thumbnail }) => {
+const Artboard = ({ id, name, thumbnail, documentId }) => {
   return (
     <Spacer as={Content} right={3} left={3} bottom={3}>
-      <Link href={`/artboard/${id}`}>
+      <Link href={`/${documentId}/artboard/${id}`}>
         <Anchor>
           <Root>
             <ImageWrap>
@@ -34,9 +34,10 @@ const Artboard = ({ id, name, thumbnail }) => {
 };
 
 Artboard.propTypes = {
-  id: PropTypes.string,
-  name: PropTypes.string,
-  thumbnail: PropTypes.string
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  documentId: PropTypes.string.isRequired
 };
 
 export default Artboard;
